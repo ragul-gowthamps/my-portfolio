a4bef5a3a4a34e8e00476593d37737ca625236de
ragul-gowthamps <ragul.gowthamps@gmail.com>
2025-03-27 20:04:13 +0000

    Initial commit - Portfolio Website

---
+++ script.js
@@ -0,0 +1,24 @@
+body {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    background-color: #f4f4f4;
+    color: #333;
+    margin: 0;
+}
+
+header {
+    background: #333;
+    color: white;
+    padding: 20px;
+}
+
+nav a {
+    color: white;
+    margin: 0 15px;
+    text-decoration: none;
+}
+
+section {
+    padding: 50px;
+    margin-top: 20px;
+}
